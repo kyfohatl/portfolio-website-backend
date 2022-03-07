9a0967979cc6e8785dd0e05d2a91dbbfe1381be7
Ehsan <ehsan.soltani71@yahoo.com>
2022-03-08 04:15:53 +1100

    Added CataLog description and images

--- frontend/src/pages/Examples.tsx
+++ frontend/src/pages/Examples.tsx
@@ -1,6 +1,15 @@
 import Navbar from "../components/Navbar"
 import "./Examples.css"
+
 import cataLogCreateAccImg from "../assets/images/catalog-create-acc.png"
+import cataLogHomeImg from "../assets/images/catalog-home.png"
+import cataLogSearchImg from "../assets/images/catalog-search.png"
+import cataLogManualContactImg from "../assets/images/catalog-manual-contact.png"
+import cataLogCreateOrgImg from "../assets/images/catalog-create-org.png"
+import cataLogMobileHomeImg from "../assets/images/catalog-mobile-home.png"
+import cataLogMobileContactImg from "../assets/images/catalog-mobile-contact.png"
+import cataLogMobileCreateContactImg from "../assets/images/catalog-mobile-create-contact.png"
+import vercelLogoImg from "../assets/images/vercel-logo.jpg"
 
 import CustomLink from "../components/CustomLink"
 import LinkBlock from "../components/LinkBlock"
@@ -27,13 +36,36 @@ export default function Examples() {
             <ul className="examples-list">
               <li>
                 <p>Creating and customizing user profiles</p>
-                <img src={cataLogCreateAccImg} alt="Creating and account" width="50%" height="50%" />
+                <img src={cataLogCreateAccImg} alt="Creating an account" width="80%" height="80%" />
               </li>
-              <li>Searching for and adding contacts on the CataLog network</li>
-              <li>Creating manual contact entries for contacts not on the CataLog network</li>
-              <li>Creating and interacting with organizations</li>
-              <li>Fully functional on both desktop and mobile devices</li>
-              <li>Deployed on Vercel Cloud</li>
+              <li>
+                <p>Categorizing and filtering added contacts</p>
+                <img src={cataLogHomeImg} alt="Home page" width="80%" height="80%" />
+              </li>
+              <li>
+                <p>Searching for and adding contacts on the CataLog network</p>
+                <img src={cataLogSearchImg} alt="Search page" width="80%" height="80%" />
+              </li>
+              <li>
+                <p>Creating manual contact entries for contacts not on the CataLog network</p>
+                <img src={cataLogManualContactImg} alt="Create manual contact page" width="80%" height="80%" />
+              </li>
+              <li>
+                <p>Creating and interacting with organizations</p>
+                <img src={cataLogCreateOrgImg} alt="Create organization page" width="80%" height="80%" />
+              </li>
+              <li>
+                <p>Fully functional on both desktop and mobile devices</p>
+                <div className="examples-mobile-images">
+                  <img src={cataLogMobileHomeImg} alt="Mobile home page" width="25%" height="25%" />
+                  <img src={cataLogMobileContactImg} alt="Mobile contact page" width="25%" height="25%" />
+                  <img src={cataLogMobileCreateContactImg} alt="Mobile create contact page" width="25%" height="25%" />
+                </div>
+              </li>
+              <li>
+                <p>Deployed in a serverless environment on Vercel Cloud</p>
+                <img src={vercelLogoImg} alt="Vercel cloud" width="80%" height="80%" />
+              </li>
             </ul>
           </article>
         </div>
