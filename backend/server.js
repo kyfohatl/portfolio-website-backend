e48ad36d172c011f6983031181e5d5fa6a1e8f6c
Ehsan <ehsan.soltani71@yahoo.com>
2022-03-15 01:16:24 +1100

    Strated work on authentication

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,21 @@ import express from "express"
 
 const app = express()
 app.use(express.json())
-app.use("/hello", (req, res) => {
-  res.send("Hello world")
+
+const users = []
+
+app.get("/users", (req, res) => {
+  res.json(users)
+})
+
+app.post("/users", (req, res) => {
+  const user = {
+    name: req.body.name,
+    password: req.body.password
+  }
+
+  users.push(user)
+  res.send("User added with name " + req.body.name)
 })
 
 app.listen(8000, () => {
