55704ffd11f1efcefba3026972253abf3b5aee51
Ehsan <ehsan.soltani71@yahoo.com>
2022-03-15 19:06:00 +1100

    Added basic authentication, without database implementation

--- backend/server.ts
+++ backend/server.ts
@@ -1,11 +1,12 @@
 import express from "express"
+import bcrypt from "bcrypt"
 
 const app = express()
 app.use(express.json())
 
 interface User {
-  name: String,
-  password: String
+  name: string,
+  password: string
 }
 
 const users: User[] = []
@@ -14,14 +15,43 @@ app.get("/users", (req, res) => {
   res.json(users)
 })
 
-app.post("/users", (req, res) => {
-  const user: User = {
-    name: req.body.name,
-    password: req.body.password
+// Create a new user with the given username and password
+app.post("/users", async (req, res) => {
+
+  try {
+    const passHash: string = await bcrypt.hash(req.body.password, 10)
+    const user: User = {
+      name: req.body.name,
+      password: passHash
+    }
+
+    users.push(user)
+    res.status(201).send("New user added")
+  } catch {
+    res.status(500).send()
+  }
+})
+
+// Login the given user with the given username and password, if correct
+app.post("/users/login", async (req, res) => {
+  // Check if username exists
+  const user = users.find(user => user.name === req.body.name)
+  if (user == null) {
+    return res.status(400).send("Username or password is incorrect")
   }
 
-  users.push(user)
-  res.send("User added with name " + req.body.name)
+  // Check if password hashes match
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      // Correct credentials
+      res.send("Login Success")
+    } else {
+      // Incorrect credentials
+      res.status(400).send("Username or password is incorrect")
+    }
+  } catch {
+    res.status(500).send()
+  }
 })
 
 app.listen(8000, () => {
