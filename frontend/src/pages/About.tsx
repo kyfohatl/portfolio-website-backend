3e423bf930be4d8f9961882a4c2d0a386bb729f5
Ehsan <ehsan.soltani71@yahoo.com>
2022-03-27 02:12:40 +1100

    Split git repo to backend and frontend repos

--- frontend/src/pages/About.tsx
+++
@@ -1,5 +0,0 @@
-export default function About() {
-  return (
-    <p>This is the about page!</p>
-  )
-}
--- frontend/src/pages/Techstack.tsx
+++
@@ -1,5 +0,0 @@
-export default function Techstack() {
-  return (
-    <p>This is the techstack page</p>
-  )
-}
