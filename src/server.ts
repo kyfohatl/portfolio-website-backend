763ffcafad3f96e1051f100b107d0e0583e7f465
Ehsan <ehsan.soltani71@yahoo.com>
2022-03-29 01:39:55 +1100

    Fixed port error

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ app.get("/users", authenticateToken, (req, res) => {
 })
 
 const port = process.env.PORT || 3000
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log("Listening on port " + port)
 })
 
