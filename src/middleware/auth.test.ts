914d7c33c3ff18d9390f346ab7724342faba3172
Ehsan <ehsan.soltani71@yahoo.com>
2022-06-30 22:51:42 +1000

    Trying to get jest working with typescript

--- src/middleware/auth.test.ts
+++ src/middleware/auth.test.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 import Token, { VerifyTokenReturn } from "../models/token"
 import TestServer from "../test/TestServer"
-import { makeAuthenticateToken } from "./auth"
+import { AuthenticatedResponse, makeAuthenticateToken } from "./auth"
 
 // Create mock verify access token function
 const verifyAccToken = jest.fn<VerifyTokenReturn, [string]>()
@@ -10,30 +10,17 @@ const authenticateToken = makeAuthenticateToken(verifyAccToken)
 
 describe("authenticateToken", () => {
   describe("Check verifyAccessToken function calls", () => {
-    it("Is called only once", () => {
+    it("Is called only once", async () => {
+      // Reset mocks
       verifyAccToken.mockReset()
-      TestServer.request(`${process.env.TEST_SERVER_ADDR}`)
+      // Get sample request and response objects
+      const response = await TestServer.request(`${process.env.TEST_SERVER_ADDR}/test/reqAndRes`, "GET")
+      const body = (await response.json()) as {req: Request, res: AuthenticatedResponse}
+      // Now test the middleware function with the sample objects
+      console.log(body)
+      authenticateToken(body.req, body.res, () => {})
+      // Finally run tests
       expect(verifyAccToken.mock.calls.length).toBe(1)
-    })
-  })
-
-  const accessToken = Token.generateAccessToken({ id: "123456" }).token
-
-  const mockReq: Request = {
-    cookies: { accessToken: accessToken },
-    headers: {}
-  } as Request
-
-  const res: Response = {
-    statusCode: -1,
-    status: (code: number) => {
-      res.statusCode = code
-    }
-  } as Response
-
-  describe("Input has a valid access token as cookie", () => {
-    it("Calls the next function", () => {
-      authenticateToken(req, res, () => { })
     })
   })
 })
--- src/test/TestServer.ts
+++ src/test/TestServer.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from "express"
 import app from "../expressApp"
 
 type TestRequestMethod = "GET" | "POST" | "PUT" | "DELETE"
@@ -6,8 +7,20 @@ export default class TestServer {
   static #app = app
   static #isInitialized = false
 
+  // Initializes additional test routes required for the test server
+  static #initializeTestRoutes() {
+    // A simple route that sends an express request and response objects back to origin for testing
+    TestServer.#app.get("/test/reqAndRes", (req: Request, res: Response) => {
+      res.json({req, res})
+    })
+  }
+
   // Initializes the test express server
   static #initialize() {
+    // Initialize any additional test routes required
+    TestServer.#initializeTestRoutes()
+
+    // Now initialize the server itself
     const PORT = process.env.TEST_SERVER_PORT || 8001
     TestServer.#app.listen(PORT, () => {
       console.log("Test server listening on port " + PORT)
@@ -16,7 +29,7 @@ export default class TestServer {
   }
 
   // Sends a request to the test server, for the given route, with the given method, headers and body
-  static async request(url: string, method: TestRequestMethod, headers: Record<string, string>, body: any) {
+  static async request(url: string, method: TestRequestMethod, headers?: Record<string, string>, body?: any) {
     // Initialize test server if it has not been initialized already
     if (!TestServer.#isInitialized) TestServer.#initialize()
 
