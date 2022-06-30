b4bb767d7aa89b598c03b19b604a292a0d70242f
Ehsan <ehsan.soltani71@yahoo.com>
2022-06-30 21:19:57 +1000

    Did not save file

--- src/middleware/auth.test.ts
+++ src/middleware/auth.test.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from "express"
-import Token from "../models/token"
-import { authenticateToken } from "./auth"
+import Token, { VerifyTokenReturn } from "../models/token"
+import TestServer from "../test/TestServer"
+import { makeAuthenticateToken } from "./auth"
+
+// Create mock verify access token function
+const verifyAccToken = jest.fn<VerifyTokenReturn, [string]>()
+// Now create express middleware function using the mock function
+const authenticateToken = makeAuthenticateToken(verifyAccToken)
 
 describe("authenticateToken", () => {
+  describe("Check verifyAccessToken function calls", () => {
+    it("Is called only once", () => {
+      verifyAccToken.mockReset()
+      TestServer.request(`${process.env.TEST_SERVER_ADDR}`)
+      expect(verifyAccToken.mock.calls.length).toBe(1)
+    })
+  })
+
   const accessToken = Token.generateAccessToken({ id: "123456" }).token
 
   const mockReq: Request = {
