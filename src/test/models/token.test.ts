4f36385feb143ebfc283b0149a383d973f4584c7
Ehsan <ehsan.soltani71@yahoo.com>
2022-07-15 15:16:11 +1000

    Added tests for Token.verifyAccToken

--- src/test/models/token.test.ts
+++ src/test/models/token.test.ts
@@ -1,5 +1,8 @@
+import Token from "../../models/token"
 import runTestEnvSetup from "../setup"
 import runTestEnvTeardown from "../teardown"
+import jwt from "jsonwebtoken"
+import { AuthUser } from "../../custom"
 
 // Run setup
 beforeAll(async () => {
@@ -16,13 +19,36 @@ jest.mock("jsonwebtoken")
 
 describe("verifyAccToken", () => {
   // Mock the verify function
-  
+  const verify = jest.mocked<(
+    token: string,
+    secretOrPublicKey: jwt.Secret
+  ) => string | jwt.JwtPayload>(jwt.verify, true)
 
   describe("When given a valid token", () => {
-    it("Returns token data along with true", () => {})
+    const SAMPLE_USER: AuthUser = { id: "id1234" }
+
+    beforeAll(() => {
+      verify.mockReset()
+      verify.mockReturnValue(SAMPLE_USER)
+    })
+
+    it("Returns token data along with true", () => {
+      const data = Token.verifyAccToken("SomeValidToken")
+      expect(data).toEqual({ isValid: true, user: SAMPLE_USER })
+    })
   })
 
   describe("When given an invalid token", () => {
-    it("Returns and object containing false", () => {})
+    beforeAll(() => {
+      verify.mockReset()
+      verify.mockImplementation(() => {
+        throw new Error()
+      })
+    })
+
+    it("Returns and object containing false", () => {
+      const data = Token.verifyAccToken("SomeInvalidToken")
+      expect(data).toEqual({ isValid: false })
+    })
   })
 })
