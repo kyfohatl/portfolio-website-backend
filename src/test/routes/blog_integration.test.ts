a5c00b345bd15d3c50ef445f8e7716e3d84868ad
Ehsan <ehsan.soltani71@yahoo.com>
2022-09-18 01:40:40 +1000

    Removed unnecessary delay

--- src/test/routes/blog_integration.test.ts
+++ src/test/routes/blog_integration.test.ts
@@ -40,9 +40,6 @@ describe("GET /", () => {
       // Create some test blogs
       for (let i = 0; i < NUM_BLOGS; i++) {
         blogIds.push(await Blog.save(user.id, BASE_HTML + i, BASE_CSS + i))
-        await new Promise<void>((resolve, reject) => {
-          setTimeout(() => resolve(), 5)
-        })
       }
     })
 
